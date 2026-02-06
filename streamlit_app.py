1e8ffbc9ae1f80e737711e2ead5de1200ac7ba91
Lucas Henrique Teixeira Coelho Dias <lucashenrique-teixeira@hotmail.com>
2026-02-05 21:59:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 
-st.title(f"Customize Your Smoothie! :cup_with_straw:")
+st.title(f"CCCustomize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
