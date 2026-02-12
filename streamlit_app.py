0ed410c40ea9cd496d1cc54a09b2995519ecfe49
Lucas Henrique Teixeira Coelho Dias <lucashenrique-teixeira@hotmail.com>
2026-02-11 22:43:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -32,7 +33,6 @@ if ingredients_list:
     # st.write(ingredients_list)
     # st.text(ingredients_list)
 
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
@@ -50,3 +50,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is Ordered, {name_on_order}!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sd_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
