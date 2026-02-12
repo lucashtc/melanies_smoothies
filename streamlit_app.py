4b909b8268429abb37a6da7a65abbed3c2b1a60b
Lucas Henrique Teixeira Coelho Dias <lucashenrique-teixeira@hotmail.com>
2026-02-11 23:00:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,10 @@ ingredients_list = st.multiselect(
     max_selections = 5,
 )
 
+my_dataframe=session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'),col('SEARCH_ON'))
+pd_df=my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+
 ingredients_string = ''
 
 if ingredients_list:
@@ -35,12 +39,15 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #sd_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
-        pd_df=my_dataframe.to_pandas()
-        st.dataframe(pd_df)
+       
 
     st.write(ingredients_string)
     
